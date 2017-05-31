d448d14fdcc4df136c3d5f96ed54c9b6477fa391
GruGi <kazanek136@gmail.com>
2017-06-01 00:50:12 +0200

    EloBuddy
    echo "# EloBuddy" >> README.md
    git init
    git add README.md
    git commit -m "first commit"
    git remote add origin https://github.com/GruGi/EloBuddy.git
    git push -u origin master

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+
+namespace SPAM
+{
+
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Loading.OnLoadingComplete += OnLoadingComplete;
+        }
+
+        private static void OnLoadingComplete(EventArgs args)
+        {
+            Game.OnNotify += eventArgs =>
+            {
+                if (eventArgs.EventId.Equals(GameEventId.OnChampionKill) && eventArgs.NetworkId == Player.Instance.NetworkId && !Chat.IsOpen && !Shop.IsOpen)
+                {
+                    Chat.Say("/all ez");
+                    Chat.Say("/masterybadge");
+                    
+                }
+            };
+            Game.OnNotify += EventArgs =>
+            {
+                if (EventArgs.EventId.Equals(GameEventId.OnChampionDie) && EventArgs.NetworkId == Player.Instance.NetworkId && !Chat.IsOpen && !Shop.IsOpen) 
+                {
+
+                    Chat.Say("/all OMG LAG");
+                    Chat.Say("/ff");
+                    Chat.Say("/masterybadge");
+
+
+                }
+            };
+        }
+
+
+    }
+}
