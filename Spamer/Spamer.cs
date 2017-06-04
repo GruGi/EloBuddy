e81e065392ad47f105fa382e3b1996725a639765
GruGi <kazanek136@gmail.com>
2017-06-04 15:21:59 +0200

    EloBuddy

--- Spamer/Spamer.cs
+++ Spamer/Spamer.cs
@@ -1,42 +1,56 @@
-﻿using System;
-using EloBuddy;
-using EloBuddy.SDK.Events;
-
-
+﻿
 namespace SPAM
 {
-
+    using System;
+    using EloBuddy;
+    using EloBuddy.SDK.Events;
+    using EloBuddy.SDK.Menu;
 
     public static class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
 
             Loading.OnLoadingComplete += OnLoadingComplete;
         }
-        
-    
+        public static Menu EventsMenu;
         private static void OnLoadingComplete(EventArgs args)
         {
+            /////////////////////////////////////////////////////
+            Chat.Print("Spamer by Grugi");
+            Chat.Print("Version 0.0.0");
+            Chat.Print("Loaded Successfully");
+            /////////////////////////////////////////////////////
+
+            EventsMenu = MainMenu.AddMenu("test", "test");
+
+
+
+
+
+
+
+
+
+
+
+
             Game.OnNotify += eventArgs =>
             {
                 if (eventArgs.EventId.Equals(GameEventId.OnChampionKill) && eventArgs.NetworkId == Player.Instance.NetworkId && !Chat.IsOpen && !Shop.IsOpen)
                 {
                     Chat.Say("/all ez");
                     Chat.Say("/masterybadge");
-
+                    // Player.DoMasteryBadge();
                 }
             };
             Game.OnNotify += EventArgs =>
             {
                 if (EventArgs.EventId.Equals(GameEventId.OnChampionDie) && EventArgs.NetworkId == Player.Instance.NetworkId && !Chat.IsOpen && !Shop.IsOpen)
                 {
-
                     Chat.Say("/all OMG LAG");
                     Chat.Say("/ff");
                     Chat.Say("/masterybadge");
-
-
                 }
             };
             Game.OnNotify += EventArgs =>
@@ -44,9 +58,6 @@ namespace SPAM
                 if (EventArgs.EventId.Equals(GameEventId.OnSurrenderFailedVotes) && EventArgs.NetworkId == Player.Instance.NetworkId && !Chat.IsOpen && !Shop.IsOpen)
                 {
                     Chat.Say("plz go surr");
-
-
-
                 }
             };
             Game.OnNotify += EventArgs =>
@@ -54,8 +65,6 @@ namespace SPAM
                 if (EventArgs.EventId.Equals(GameEventId.OnSurrenderVoteAlready) && EventArgs.NetworkId == Player.Instance.NetworkId && !Chat.IsOpen && !Shop.IsOpen)
                 {
                     Chat.Say("/all REPORT MY TEAM");
-
-
                 }
             };
             Game.OnNotify += EventArgs =>
@@ -63,9 +72,6 @@ namespace SPAM
                 if (EventArgs.EventId.Equals(GameEventId.OnKillDragon) && EventArgs.NetworkId == Player.Instance.NetworkId && !Chat.IsOpen && !Shop.IsOpen)
                 {
                     Chat.Say("Good Job");
-
-
-
                 }
             };
             Game.OnNotify += EventArgs =>
@@ -73,9 +79,6 @@ namespace SPAM
                 if (EventArgs.EventId.Equals(GameEventId.OnKillDragonSteal) && EventArgs.NetworkId == Player.Instance.NetworkId && !Chat.IsOpen && !Shop.IsOpen)
                 {
                     Chat.Say("/all Report jungler ");
-
-
-
                 }
             };
             Game.OnNotify += EventArgs =>
@@ -83,28 +86,27 @@ namespace SPAM
                 if (EventArgs.EventId.Equals(GameEventId.OnMinionsSpawn) && EventArgs.NetworkId == Player.Instance.NetworkId && !Chat.IsOpen && !Shop.IsOpen)
                 {
                     Chat.Say("/all CHARGE MY MINIONS!!!");
-
-
-
                 }
             };
             Game.OnNotify += EventArgs =>
             {
                 if (EventArgs.EventId.Equals(GameEventId.OnFirstBlood) && EventArgs.NetworkId == Player.Instance.NetworkId && !Chat.IsOpen && !Shop.IsOpen)
                 {
-                    Chat.Print("");
+
+                }
+            };
+            Game.OnNotify += EventArgs =>
+            {
+                if (EventArgs.EventId.Equals(GameEventId.OnAce) && EventArgs.NetworkId == Player.Instance.NetworkId && !Chat.IsOpen && !Shop.IsOpen)
+                {
 
                 }
             };
 
-
-
-
-
-
-         }
+        }
 
 
     }
 }
+
 
